1ee7dcf9e04df2c32e9eb3a2a3de5efb44d48382
muescha <184316+muescha@users.noreply.github.com>
2024-08-09 23:35:36 +0200

    create package-userscript.sh

---
+++ package-userscript.sh
@@ -0,0 +1,45 @@
+#!/bin/zsh
+# Created by: raingart, muescha
+
+# AFTER build in vscode
+# 1. Clear comments. regex - "//\s.*|/\*[\s\S\n]*?\*/"
+# 2. For clear spaces use "Format Document".
+# 3. Replace "\n^\n+" to"\n"
+
+# outFile="$TMPDIR/nova-tube.user.js"
+outFile="nova-tube.user.js"
+rm -f "$outFile"
+
+fileArray=(
+  "./Userscript/meta.js"
+  "./Userscript/compatibility.js"
+  "./Userscript/plugin-container.js"
+)
+
+# Include additional files from ./plugins, excluding those starting with "-"
+fileArray+=($(find ./plugins -type f -name "*.js" ! -name "-*.js"))
+
+fileArray+=(
+  "./js/plugins.js"
+  "./Userscript/user.js"
+)
+
+{
+  for file in "${fileArray[@]}"; do
+
+    echo "Including file: $file" >&2  # Print the filename to the console (stderr)
+
+    print ""
+    print ""
+    print "/* --------------- */"
+    print "/* file: $file */"
+    print "/* --------------- */"
+    print ""
+    cat "$file"  # Append the file content to the output file
+  done
+} > "$outFile"
+
+# Print the output file path to the console
+echo ""
+echo "user file created:"
+echo "$outFile"
